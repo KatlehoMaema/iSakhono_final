6852ee868ab68171a0e198008ee2834667436373
Kgotso Maema <lolwethu0988@mail.com>
2023-06-15 16:14:32 +0200

    created routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './components/user/signup/signup.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { JobSearchComponent } from './components/user/job-search/job-search.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "", redirectTo: "home", pathMatch: "full"},
+  {
+    path: "home", component: HomeComponent
+  },
+  {
+    path: "register-user", component: SignupComponent
+  },
+  {
+    path: "user-profile", component: ProfileComponent
+  },
+  {
+    path: "search-job", component: JobSearchComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
