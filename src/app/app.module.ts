0192d26a9864e8df6043e2a15779ffce82d7ccac
Kgotso Maema <lolwethu0988@mail.com>
2023-06-15 15:42:10 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegiserComponent } from './components/user/regiser/regiser.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { ProfileEditComponent } from './components/user/profile-edit/profile-edit.component';
+import { JobSearchComponent } from './components/user/job-search/job-search.component';
+import { CompanyProfileComponent } from './components/company/company-profile/company-profile.component';
+import { CandidateSearchComponent } from './components/company/candidate-search/candidate-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegiserComponent,
+    LoginComponent,
+    ProfileComponent,
+    ProfileEditComponent,
+    JobSearchComponent,
+    CompanyProfileComponent,
+    CandidateSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/company/candidate-search/candidate-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-search',
+  templateUrl: './candidate-search.component.html',
+  styleUrls: ['./candidate-search.component.scss']
+})
+export class CandidateSearchComponent {
+
+}
