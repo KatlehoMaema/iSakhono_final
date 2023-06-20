850d4eb0f3c6c573fc0b193957dcce51a3c0fd74
Vuyo Sopangisa <vssopangisa@gmail.com>
2023-06-20 17:24:29 +0200

    feature done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RegiserComponent } from './components/user/regiser/regiser.component';
+import { LoginComponent } from './components/user/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RegiserComponent,
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'regiser', component: RegiserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  declarations: [],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
