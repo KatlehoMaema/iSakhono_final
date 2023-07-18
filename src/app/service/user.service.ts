aec3054ea6dce23a9943e9d05ae95189e350e192
Uviwe Dana <uviwedana09@gmail.com>
2023-07-18 16:22:32 +0200

    updates

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,23 +1,33 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const user_api = "https://i-sakhono-backend.vercel.app/api/users/"
-@Injectable()
+const token = window.sessionStorage.getItem("token") ? window.sessionStorage.getItem("token") : null
+
+let httpOptions = {
+  headers: new HttpHeaders({ 
+    'x-access-token': `${token}` 
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
 export class UserService {
-  private userDataSource = new BehaviorSubject({email : '', password : ''});
-  currentUserData = this.userDataSource.asObservable();
+
   constructor(
     private http: HttpClient
   ) { }
-  changeData(newUserData:any)  {
-    this.userDataSource.next(newUserData)
-  }
+
   getUser(data: any, id: any){
     return this.http.get(user_api+id)
   }
   updateUser(data: any, id: any){
     return this.http.put(user_api+id, data)
   }
-  
+
+  getAllUsers(){
+    
+    return this.http.get(user_api, httpOptions)
+  }
 }
